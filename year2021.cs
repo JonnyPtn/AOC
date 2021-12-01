69a630955752ff38e381353f90c2a5f57ec912c4
Jonny Paton <jonathan.r.paton@googlemail.com>
2021-12-01 10:53:12 +0000

    Add code to fetch and cache input and verify output, along with 2021 day 1 solutions

---
+++ year2021.cs
@@ -0,0 +1,59 @@
+﻿namespace Year2021
+{
+    public class Day1
+    {
+        public string solve1(string input)
+        {
+            // We go through each entry and count how many increased from the previous entry
+            int increaseCount = 0;
+            int? previousMeasurement = null;
+            var reader = new StringReader(input);
+            for (string? line; (line = reader.ReadLine()) != null;)
+            {
+                var value = int.Parse(line);
+                if (previousMeasurement != null)
+                {
+                    if (value > previousMeasurement)
+                    {
+                        ++increaseCount;
+                    }
+                }
+                previousMeasurement = value;
+            }
+            return increaseCount.ToString();
+        }
+
+        public string solve2(string input)
+        {
+            // Now we do the same but for a sliding group of three values
+            int increaseCount = 0;
+            Queue<int> group = new Queue<int>();
+            int? previousMeasurement = null;
+            var reader = new StringReader(input);
+            for (string? line; (line = reader.ReadLine()) != null;)
+            {
+                var value = int.Parse(line);
+                group.Enqueue(value);
+                if (group.Count > 3)
+                {
+                    group.Dequeue();
+                }
+
+                if (group.Count == 3)
+                { 
+                    int sum = 0;
+                    foreach (int i in group)
+                    {
+                        sum += i;
+                    }
+                    if (previousMeasurement != null && sum > previousMeasurement)
+                    {
+                        ++increaseCount;
+                    }
+                    previousMeasurement = sum;
+                }
+            }
+            return increaseCount.ToString();
+        }
+    }
+}
