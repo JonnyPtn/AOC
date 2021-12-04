6d1e1de90ff3c6cfcbeecf036e897cd5569f05c6
Jonny Paton <jonathan.r.paton@googlemail.com>
2021-12-04 14:04:07 +0000

    Day 3 solutions, pretty gross…

--- year2021.cs
+++ year2021.cs
@@ -40,7 +40,7 @@
                 }
 
                 if (group.Count == 3)
-                { 
+                {
                     int sum = 0;
                     foreach (int i in group)
                     {
@@ -109,6 +109,79 @@
                 }
             }
             return (horizontal * depth).ToString();
+        }
+    }
+
+    public class Day3
+    {
+        System.Collections.BitArray getMostCommonBits(string[] input, bool reverse)
+        {
+            var bitCounts = new List<int>();
+            foreach (var line in input)
+            {
+                int index = 0;
+                foreach (var bit in line)
+                {
+                    if (index >= bitCounts.Count())
+                    {
+                        bitCounts.Add(0);
+                    }
+                    if (bit == '1')
+                    {
+                        ++bitCounts[index];
+                    }
+                    ++index;
+                }
+            }
+            var mostCommonBits = new System.Collections.BitArray(bitCounts.Count(), false);
+            var bitToSet = 0;
+            if (reverse)
+            {
+                bitCounts.Reverse();
+            }
+            foreach (var count in bitCounts)
+            {
+                if ((float)count >= (float)input.Count() / 2)
+                {
+                    mostCommonBits[bitToSet] = true;
+                }
+                ++bitToSet;
+            }
+            return mostCommonBits;
+        }
+
+        public string solve1(string input)
+        {
+            var mostCommonBits = getMostCommonBits(input.Split('\n'), true);
+            var rates = new int[2];
+            mostCommonBits.CopyTo(rates, 0); // gamma
+            mostCommonBits.Not().CopyTo(rates, 1); // epsilon
+            return (rates[0] * rates[1]).ToString();
+        }
+
+        public string solve2(string input)
+        {
+            var lines = new List<string> (input.Split('\n'));
+            lines.RemoveAll(line => line.Length == 0);
+            var o2lines = new List<string>(lines);
+            var co2lines = new List<string>(lines);
+
+            int index = 0;
+            while(o2lines.Count() > 1)
+            {
+                var mostCommonBits = getMostCommonBits(o2lines.ToArray(), false);
+                o2lines.RemoveAll(line => line[index] != (mostCommonBits[index] ? '1' : '0'));
+                ++index;
+            }
+
+            index = 0;
+            while (co2lines.Count() > 1)
+            {
+                var mostCommonBits = getMostCommonBits(co2lines.ToArray(), false);
+                co2lines.RemoveAll(line => line[index] != (mostCommonBits[index] ? '0' : '1'));
+                ++index;
+            }
+            return (Convert.ToUInt32(o2lines[0],2) * Convert.ToUInt32(co2lines[0],2)).ToString(); ;
         }
     }
 }
