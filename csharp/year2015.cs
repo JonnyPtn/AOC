d65110694b9087d2520b3426be38ea73f0707f76
JonnyPtn <jonathan.r.paton@googlemail.com>
2024-11-29 09:01:55 +0000

    2015 day 3

--- csharp/year2015.cs
+++ csharp/year2015.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Numerics;
-using System.Text.RegularExpressions;
-using System.Xml.Schema;
+﻿using System.Drawing;
 
 namespace Year2015
 {
@@ -14,6 +11,7 @@ namespace Year2015
             var down = input.Count(f => f == ')');
             return (up - down).ToString();
         }
+
         public string solve2(string input)
         {
             // Find the first time the floor goes negative
@@ -29,11 +27,13 @@ namespace Year2015
                 {
                     floor--;
                 }
+
                 if (floor < 0)
                 {
                     return (i + 1).ToString();
                 }
             }
+
             return input.Length.ToString();
         }
     }
@@ -54,8 +54,10 @@ namespace Year2015
                 var area = 2 * l * w + 2 * w * h + 2 * h * l;
                 total += area + new[] { l * w, w * h, h * l }.Min();
             }
+
             return total.ToString();
         }
+
         public string solve2(string input)
         {
             //The ribbon required to wrap a present is the shortest distance around its sides
@@ -73,7 +75,55 @@ namespace Year2015
                 values.Remove(values.Max());
                 total += values[0] * 2 + values[1] * 2 + bowLength;
             }
+
             return total.ToString();
+        }
+    }
+
+    public class Day3
+    {
+        public HashSet<Point> getVisitedHouses(string input)
+        {
+            // ^ v < > directions, count locations we hit
+            Point position = new();
+            HashSet<Point> visitedHouses = new() { position };
+            foreach (var direction in input)
+            {
+                switch (direction)
+                {
+                    case '^':
+                        position.Y++;
+                        break;
+                    case 'v':
+                        position.Y--;
+                        break;
+                    case '<':
+                        position.X--;
+                        break;
+                    case '>':
+                        position.X++;
+                        break;
+                }
+                visitedHouses.Add(position);
+            }
+
+            return visitedHouses;
+        }
+        public string solve1(string input)
+        {
+            return getVisitedHouses(input).Count.ToString();
+        }
+
+        public string solve2(string input)
+        {
+            // Now there's two santas, taking alternate turns,
+            // so split the input accordingly then count and combine
+            var a = new string(input.Where((c,i) => i % 2 == 0).ToArray());
+            var b = new string(input.Where((c,i) => i % 2 != 0).ToArray());
+            var av = getVisitedHouses(a);
+            var bv = getVisitedHouses(b);
+            av.UnionWith(bv);
+            return av.Count().ToString();
         }
     }
 }
